f99feb4d1b8cf8231f6a5808ad42d4a1549226cc
mkurtis01 <mkurtis01@manhattan.edu>
2024-06-14 16:42:40 +0200

    System working! Basic functionality achieved

--- Main.py
+++ Main.py
@@ -1,13 +1,32 @@
 from pixelsort import pixelsort
 from PIL import Image
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 
-print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Please insert tip.")
+
+print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Please insert tip.\n\n")
+
+
+initialName = input("What is the name of the image you want to fuck up?\n")
+fileType = input("What is the file type of the image you want to fuck up? If you already input a file type, don't input anything.\n")
+finalName = input("What do you want to call the fucked up image?\n")
+
+
+#determine what file will be sorted, and what it should be called
+input_path = os.getenv("input_path") + initialName
+output_path = os.getenv("output_path") + finalName + ".png"
+
 
 #Pixelsorting
-path = input("What is the path to the image you want to fuck up?")
+path = r"C:\Users\usuario\Desktop\thrushArt\images\Unsorted\download.png"
 theImage = Image.open(path) #insert the image you want here.
-theImage
+
+
 sortedImage = pixelsort(theImage)
-sortedImage
+
+
+sortedImage.save(output_path)
+print("image has been saved to " + output_path)
