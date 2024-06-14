bca30f65a715d97f14d23f34c96bd8d5566aed5a
mkurtis01 <mkurtis01@manhattan.edu>
2024-06-14 15:09:42 +0200

    testing from desktop

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Plea
 
 #Pixelsorting
 path = input("What is the path to the image you want to fuck up?")
-theImage = Image.open() #insert the image you want here.
+theImage = Image.open(path) #insert the image you want here.
 theImage
 sortedImage = pixelsort(theImage)
 sortedImage
