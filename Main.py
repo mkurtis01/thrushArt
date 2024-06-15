d53d200a0703b5f2ab3a4425fd3584db666cfcb5
johnny-appleseed-69420 <71612886+johnny-appleseed-69420@users.noreply.github.com>
2024-06-15 05:35:34 -0400

    Script tested, working on Windows Macbook

--- Main.py
+++ Main.py
@@ -10,23 +10,30 @@ load_dotenv()
 print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Please insert tip.\n\n")
 
 
-initialName = input("What is the name of the image you want to fuck up?\n")
-fileType = input("What is the file type of the image you want to fuck up? If you already input a file type, don't input anything.\n")
+initialName = input("What is the name of the image you want to fuck up? (Case-Sensitive)\n")
+fileType = input("What is the file type of the image you want to fuck up? If you already input a file type, just hit enter.\n")
 finalName = input("What do you want to call the fucked up image?\n")
 
 
-#determine what file will be sorted, and what it should be called
-input_path = os.getenv("input_path") + initialName
+#determine what file will be sorted, and what it should be called.  Uses .env to get paths.
+input_path = os.getenv("input_path") + initialName + fileType
 output_path = os.getenv("output_path") + finalName + ".png"
 
 
 #Pixelsorting
-path = r"C:\Users\usuario\Desktop\thrushArt\images\Unsorted\download.png"
-theImage = Image.open(path) #insert the image you want here.
+print(f"Attempting to open: {input_path}")
 
+try:
 
-sortedImage = pixelsort(theImage)
+    theImage = Image.open(input_path) #insert the image you want here.
 
+    sortedImage = pixelsort(theImage)
 
-sortedImage.save(output_path)
-print("image has been saved to " + output_path)
+    sortedImage.save(output_path)
+    print("image has been saved to " + output_path)
+
+except OSError:
+    print("OSError, File not found")
+
+except:
+    print("Something went wrong")
